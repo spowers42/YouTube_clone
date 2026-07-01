89be4b522b072fdea40688552629f4b104d7098e
Scott Powers <scott@Scotts-MacBook-Pro.local>
2026-07-01 08:45:23 -0400

    Initial service implementation
    Initial implementation providing endpoints for
    * downscaling to 360p
    * extracting the audio
    * extracting a thumbnail inage

---
+++ video-processing-service/src/index.ts
@@ -0,0 +1,100 @@
+import express from 'express';
+import ffmpeg from 'fluent-ffmpeg';
+
+const app = express();
+app.use(express.json());
+
+const port = process.env.port || 3000;
+
+app.post('/downScale', (req, res) => {
+    const inputFilePath = req.body.inputFilePath;
+    const outputFilePath = req.body.outputFilePath;
+
+    if (!inputFilePath){
+        return res.status(400).send("Input file path required");
+    }
+    if (!outputFilePath){
+        return res.status(400).send("Output file path required");
+    }
+
+    ffmpeg(inputFilePath)
+    .outputOptions('-vf', 'scale=-1:360')
+    .on('end', function(){
+        console.log("processing finished");
+        res.status(200).send('processing finished successfully');
+    })
+    .on('error', function(err: any){
+        let message = "error processing video: " + err.message;
+        console.log(message);
+        res.status(500).send(message);
+    })
+    .save(outputFilePath);
+
+});
+
+app.post('/extractAudio', (req, res) => {
+    const inputFilePath = req.body.inputFilePath;
+    const outputFilePath = req.body.outputFilePath;
+
+    if (!inputFilePath){
+        return res.status(400).send("Input file path required");
+    }
+    if (!outputFilePath){
+        return res.status(400).send("Output file path required");
+    }
+    if (!outputFilePath.endsWith(".mp3")){
+        return res.status(400).send("Output file path must be .mp3")
+    }
+
+    // TODO fix: this fails on files with no audio stream 
+
+    ffmpeg(inputFilePath)
+    .outputOptions('-vn', '-q:a 0')
+    .on('end', function(){
+        console.log('audio extracted');
+        res.status(200).send('audio extraction finished successfully');
+    })
+    .on('error', function(err: any){
+        let message = 'error extracting audio: ' + err.message;
+        console.log(message);
+        res.status(500).send(message);
+    })
+    .save(outputFilePath);
+});
+
+app.post('/extractThumbnail', (req, res) => {
+    const inputFilePath = req.body.inputFilePath;
+    const outputFilePath = req.body.outputFilePath;
+
+    if (!inputFilePath){
+        return res.status(400).send("Input file path required");
+    }
+    if (!outputFilePath){
+        return res.status(400).send("Output file path required");
+    }
+    if (!outputFilePath.endsWith(".jpg")){
+        return res.status(400).send("Output file path must be .jpg")
+    }
+
+    ffmpeg(inputFilePath)
+    .seekInput(10)
+    .size("?x360")
+    .aspect("4:3")
+    .autoPad()
+    .outputOptions('-vframes:v 1')
+    .on("end", function(){
+        console.log('thumbnail extracted');
+        res.status(200).send("thumbnail successfull extracted");
+    })
+    .on("error", function(err: any){
+        let message = "error extracting thumbnail: " + err.message;
+        console.log(message);
+        res.status(500).send(message);
+    })
+    .save(outputFilePath);
+});
+
+
+app.listen(port, () =>{
+    console.log(`server starting on http://localhost:${port}`);
+});
